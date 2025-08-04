ce3570219b0ce6fad0e6096f956453151e8bba98
Viktor Shcherbakov <viktoroo.sch@gmail.com>
2025-08-04 19:47:48 +0200

    add n_renders to example

--- example.py
+++ example.py
@@ -7,8 +7,14 @@ st.write("Press any key on your keyboard")
 if "history" not in st.session_state:
     st.session_state["history"] = []
 
+if "n_renders" not in st.session_state:
+    st.session_state["n_renders"] = 0
+
 key = key_press_events()
 if key:
     st.session_state["history"].append(key)
 
 st.write(f"Pressing history: {st.session_state['history']}")
+st.write(f"Number of renders: {st.session_state['n_renders']}")
+
+st.session_state['n_renders'] += 1
